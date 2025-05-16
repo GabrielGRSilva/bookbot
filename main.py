e8eed918a7fd2332fe5ab92af460bfee092c2379
GabrielGRSilva <gabrielgrsilva@outlook.com.br>
2025-05-15 21:50:00 -0300

    adding final touches to the bookbot

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-from stats import count_words, count_characters
+from stats import count_words, count_characters, sort_dictionaries
 
-def get_book_text (path):
-    with open(path) as f:
+def get_book_text (path):                ##Receives the text to be used in other functions
+    with open(path) as f:                
         book_text = f.read()
         return book_text
 
 def main ():
     text = get_book_text("books/frankenstein.txt")
-    count_words("books/frankenstein.txt") #this counts words
-    char_number = count_characters(text)
-    print (char_number)
+    number_of_words = count_words("books/frankenstein.txt")
+    char_number = count_characters(text)         
+    value_of_chars = sort_dictionaries(char_number)
+    print ("============ BOOKBOT ============")
+    print ("Analyzing book found at books/frankenstein.txt...")
+    print (f"Found {number_of_words} total words")
+    print ("--------- Character Count -------")
+    for x in value_of_chars:
+        print(f"{x['char']}: {x['num']}")
+    print ("============= END ===============")
+
 
 main()
--- stats.py
+++ stats.py
@@ -2,7 +2,7 @@ def count_words (text):
         with open(text) as f:
             count_words = f.read() ##this reads the book and transforms it into a list
             word_list = count_words.split()
-            print (f"{len(word_list)} words found in the document")
+            return len(word_list)
 
 def count_characters (text):
         lowered_characters = text.lower()
@@ -13,3 +13,35 @@ def count_characters (text):
             else:
                 count_dict[char] = 1
         return count_dict
+
+def get_dict_num(num_key):
+    return num_key["num"]
+
+def sort_dictionaries (char_number):
+    char_number_list = []
+    for char, num in char_number.items():
+         dict_char_num = {}
+         dict_char_num["char"] = char
+         dict_char_num["num"] = num
+         if char.isalpha():
+            char_number_list.append(dict_char_num)
+    char_number_list.sort(reverse=True, key=get_dict_num)
+    return char_number_list     
+
+  
+  
+  
+  
+  
+  #  characters={}
+   # values={}
+    #dict_list=[]
+    #for char, num in char_number.items():
+     #   characters[char] = char
+      #  values[num] = num
+       # dict_list.append(characters[char], values[num])
+                         
+        #dict_list=[characters[char],values[num]]
+        
+        #dict_list.sort(reverse=True, key=num)
+    #return dict_list
