863a85d39de93bce3db2dbfb1dbfab06623f245a
GabrielGRSilva <gabrielgrsilva@outlook.com.br>
2025-05-17 17:47:01 -0300

    RELEASE: First version is ready for Ubuntu

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 from stats import count_words, count_characters, sort_dictionaries
+import sys
+
+print (sys.argv)
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+book_path = sys.argv[1]
 
 def get_book_text (path):                ##Receives the text to be used in other functions
     with open(path) as f:                
         book_text = f.read()
-        return book_text
+    return book_text
 
 def main ():
-    text = get_book_text("books/frankenstein.txt")
-    number_of_words = count_words("books/frankenstein.txt")
+    text = get_book_text(book_path)
+    number_of_words = count_words(book_path)
     char_number = count_characters(text)         
     value_of_chars = sort_dictionaries(char_number)
     print ("============ BOOKBOT ============")
-    print ("Analyzing book found at books/frankenstein.txt...")
+    print ("Analyzing book found at {book_path}" )
     print (f"Found {number_of_words} total words")
     print ("--------- Character Count -------")
     for x in value_of_chars:
